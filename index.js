89112d12b3eb7643067216681c6282806d3ff6b7
zubin.pratap@gmail.com <zubin.pratap@gmail.com>
2018-05-27 07:45:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  
 
   // Feel free to move things around!
-  const two = 2
+  const two = 2;
+  return n + two;
 }
 
 var funkyFunction = function() {
